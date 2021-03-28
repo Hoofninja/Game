26b1ad35deb5625fb8d4bc1f59aa502963f5ef94
Mazilkin Ivan <Mograine@Yandex.ru>
2021-03-28 14:17:02 +0300

    Player sprite added

--- main.py
+++ main.py
@@ -29,20 +29,51 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
 
 
 # Walls sprites
 class Wall(pygame.sprite.Sprite):
+
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.image = pygame.Surface((30, 30))
-        self.image.fill(BLUE)
+        self.image.fill(GRAY)
         self.rect = self.image.get_rect()
         self.rect.left = x
         self.rect.top = y
         self.speed = 0
+
+
+# Player sprite
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (len(xy_map[0])*30/2, len(xy_map)*30/2)
+        self.speed_x = 0
+        self.speed_y = 0
+
+    def update(self):
+        self.speed_x = 0
+        self.speed_y = 0
+        key_state = pygame.key.get_pressed()
+        if key_state[pygame.K_LEFT]:
+            self.speed_x = -8
+        if key_state[pygame.K_RIGHT]:
+            self.speed_x = 8
+        if key_state[pygame.K_UP]:
+            self.speed_y = -8
+        if key_state[pygame.K_DOWN]:
+            self.speed_y = 8
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
 
 # Create game window
 pygame.init()
@@ -51,14 +82,17 @@ screen = pygame.display.set_mode((len(xy_map[0])*30, len(xy_map)*30))
 pygame.display.set_caption('My Game')
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
+
+# Wall creation
 for i in range(len(xy_map)):
     for j in range(len(xy_map[i])):
         if xy_map[i][j] == 'x':
             wall = Wall(j*30,i*30)
             all_sprites.add(wall)
 
-
-
+# Player creation
+player = Player()
+all_sprites.add(player)
 
 # Game cycle
 FPS = 30
